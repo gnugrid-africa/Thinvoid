bc24064b5449efe8569acef755ace9a057423380
David Opio Obwangamoi <49604143+obwangamoi@users.noreply.github.com>
2019-10-27 01:34:36 +0200

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("extract_sensor_data.php"); ?>
-<?php
-  $api = new extract_sensor_data();
-
-  //$api->send_sms("256782157074","Testing","6115");
-  //$api->trigger_device("00211676","turn_on");
-
-  $api->trigger_device("00211676","turn_off");
-?>
